ad1ef0f5b80e8a063627eb983686101d6466d5d1
techehub <vijeesh.tp@techehub.com>
2019-11-12 15:24:29 +0530

    Profile anf Log commits

--- src/main/java/com/coderzon/citycountry/MyConytroller.java
+++ src/main/java/com/coderzon/citycountry/MyConytroller.java
@@ -3,12 +3,22 @@ package com.coderzon.citycountry;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 @RestController
 public class MyConytroller {
 
+	@Value("${databasename}") String name;
+	
+	private static final Logger logger = LoggerFactory.getLogger(MyConytroller.class);
+	   
+	
 	@GetMapping("/countries")
 	public  List<Country> getCountries() {
 		List<Country> countries = new ArrayList<>();
@@ -16,7 +26,40 @@ public class MyConytroller {
 		countries.add(new Country("2", "USA"));
 		countries.add(new Country("3", "UK"));
 		countries.add(new Country("4", "Canada"));
+		
+		logger.info("this is a info message");
+	    logger.warn("this is a warn message");
+	    logger.error("this is a error message");
+		
+	    
+		try {
+			int i=0;
+			int x= 10/i;
+		}
+		catch (Exception e) {
+			logger.error("ERROR", e);
+		}
+		
+		
 		return countries;
+	
+	}
+	
+	@GetMapping ("/students")
+	public List<Student> getStudents (){
+		List<Student> students = new ArrayList<>();
+		
+		students.add(new Student("Anil", "sadasd@sddff.com"));
+		students.add(new Student("Ashraf", "sadasd@sddff.com"));
+		students.add(new Student("Neenu", "sadasd@sddff.com"));
+		
+		return students;
+	}
+	
+	
+	@GetMapping ("/dbname")
+	public String dbdetails (){		
+		return name;
 	}
 	
 }
---
+++ src/main/java/com/coderzon/citycountry/Student.java
@@ -0,0 +1,38 @@
+package com.coderzon.citycountry;
+
+public class Student {
+
+String name;
+
+String email;
+
+
+
+
+public Student(String name, String email) {
+	super();
+	this.name = name;
+	this.email = email;
+}
+
+public String getName() {
+	return name;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+
+public String getEmail() {
+	return email;
+}
+
+public void setEmail(String email) {
+	this.email = email;
+}
+	
+
+
+
+}
+
